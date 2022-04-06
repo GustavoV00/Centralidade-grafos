af26bbbe5dcd160a0791aca9a43d606a58364daa
Gustavo Valente <gustavo.pw00@gmail.com>
2022-04-06 12:00:58 -0300

    Update graph.c and graph.h
    Starting reading the input graph.

--- includes/grafo.h
+++ includes/grafo.h
@@ -5,59 +5,74 @@
 
 //------------------------------------------------------------------------------
 // (apontador para) estrutura de dados para representar um grafo
-// 
+//
 // o grafo tem um nome, que é uma "string"
-
-typedef struct grafo *grafo;
 
 //------------------------------------------------------------------------------
 // (apontador para) estrutura de dados para representar um vértice
-// 
+//
 // o vértice tem um nome, que é uma "string"
 
-typedef struct vertice *vertice;
+typedef struct vertice_t {
+  char v;
+  struct vertice_t *vizinhos;
 
+} vertice_t;
+
+// typedef struct grafo_t {
+//   struct vertice_t *lista_adj;
+
+// } grafo_t;
+
+typedef struct queue_grafo_t {
+  struct queue_t *prev;
+  struct queue_t *next; // Em vez de utilizar uma matriz para representar o
+                        // grafo, estou utilizando uma fila. Sendo que cada nodo
+                        // dessa lista, é  eferente a um id. a, b, c, ..., z
+  struct vertice_t vertice;
+  struct vertice_t
+      *lista_adj;   // Lista que contem os vértices, que o nodo x fazem parte
+  int num_vertices; // Indica qual o tamanho da lista_adj no nodo id
+
+} queue_grafo_t;
 //------------------------------------------------------------------------------
 // desaloca toda a memória usada em *g
-// 
+//
 // devolve 1 em caso de sucesso,
-//         ou 
+//         ou
 //         0, caso contrário
 
-int destroi_grafo(grafo g);
+int destroi_grafo(queue_grafo_t g);
 
 //------------------------------------------------------------------------------
 // lê um grafo de input.
 // um grafo é um arquivo onde cada linha tem zero, uma ou duas
 // strings (sequência de caracteres sem "whitespace").
-// 
+//
 // devolve o grafo lido. Caso o arquivo esteja mal formado o
 // comportamento da função é indefinido
 
-grafo le_grafo(FILE *input);  
-
-
+queue_grafo_t *le_grafo(FILE *input);
 
 //------------------------------------------------------------------------------
 // pede para o usuário entrar com um vértice.
 
-vertice le_vertice(void);  
+vertice_t le_vertice(void);
 
 //------------------------------------------------------------------------------
 // escreve o grafo g em output, no mesmo formato que o usado por le_grafo()
 //
 // devolve o grafo escrito,
-//         ou 
-//         NULL, em caso de erro 
-
+//         ou
+//         NULL, em caso de erro
 
 //------------------------------------------------------------------------------
 // devolve o coeficiente de agrupamento de g
-// ou seja, o número de tríades fechadas divido pelo 
+// ou seja, o número de tríades fechadas divido pelo
 // total de tríades (ou seja, a soma de tríades abertas e fechadas).
-// 
+//
 
-double coeficiente_proximidade(grafo g, vertice v);
+double coeficiente_proximidade(queue_grafo_t g, vertice_t v);
 
 //------------------------------------------------------------------------------
 #endif
--- src/grafo.c
+++ src/grafo.c
@@ -1,65 +1,74 @@
-#include <stdio.h>
-#include "grafo.h"
+#include "../includes/grafo.h"
+#include <stdlib.h>
 
 //------------------------------------------------------------------------------
 // (apontador para) estrutura de dados para representar um grafo
-// 
+//
 // o grafo tem um nome, que é uma "string"
 
 typedef struct grafo *grafo;
 
 //------------------------------------------------------------------------------
 // (apontador para) estrutura de dados para representar um vértice
-// 
+//
 // o vértice tem um nome, que é uma "string"
 
 typedef struct vertice *vertice;
 
 //------------------------------------------------------------------------------
 // desaloca toda a memória usada em *g
-// 
+//
 // devolve 1 em caso de sucesso,
-//         ou 
+//         ou
 //         0, caso contrário
 
-int destroi_grafo(grafo g) {
+// int destroi_grafo(queue_grafo_t g) { return g == NULL; }
 
-  return g == NULL;
+queue_grafo_t *inclui_nodo_na_fila(char v1, char v2, queue_grafo_t *g) {
+  queue_grafo_t *novo_vertice = (queue_grafo_t *)malloc(sizeof(queue_grafo_t));
+  novo_vertice->prev = NULL;
+  novo_vertice->next = NULL;
+
+  return g;
 }
 
 //------------------------------------------------------------------------------
 // lê um grafo
-// 
+//
 // devolve o grafo lido,
-//         ou 
-//         NULL, em caso de erro 
+//         ou
+//         NULL, em caso de erro
 
-grafo le_grafo(FILE *input) {
+queue_grafo_t *le_grafo(FILE *input) {
+  queue_grafo_t *grafo = NULL;
 
-  return (grafo)input;
+  FILE *f = input;
+
+  char v1, v2;
+  while (!feof(f)) {
+    fscanf(f, "%c %c ", &v1, &v2);
+    inclui_nodo_na_fila(v1, v2, grafo);
+    printf("%c e %c\n", v1, v2);
+  }
+
+  fclose(f);
+
+  return grafo;
 }
-
 
 //------------------------------------------------------------------------------
 // lê um vertice 
 
-vertice le_vertice(void) {
-
-  return (vertice) NULL;
-}
-
-
-
+// vertice le_vertice(void) { return (vertice)NULL; }
 
 //------------------------------------------------------------------------------
 // devolve o coeficiente de proximidade do vértice v de g
-// 
+//
 
-double coeficiente_proximidade(grafo g, vertice v) {
-	
-  v = NULL;
-  g = NULL;
+// double coeficiente_proximidade(grafo g, vertice v) {
 
-  return !(v == (vertice) g);
-}
+//   v = NULL;
+//   g = NULL;
 
+//   return !(v == (vertice)g);
+// }
