fd63a9738d70c1e090607977b30a15361eab9f2b
Gustavo Valente <gustavo.pw00@gmail.com>
2022-04-07 20:44:21 -0300

    Update teste.c
    Ajuste do lixo

--- src/teste.c
+++ src/teste.c
@@ -1,32 +1,35 @@
 #include "../includes/grafo.h"
 #include "../includes/queue.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define STRING_SIZE 1024
 
 //------------------------------------------------------------------------------
 
-int main(void)
-{
-	vertice_t v;
-	double coef;
+int main(void) {
+  vertice_t v;
+  double coef;
 
-	queue_grafo_t *grafo = le_grafo(stdin);
-	// queue_print("Elementos inseridos:\n", (queue_t *)grafo, print_fila);
-	// v = le_vertice();
+  queue_grafo_t *grafo = le_grafo(stdin);
+  // queue_print("Elementos inseridos:\n", (queue_t *)grafo, print_fila);
+  // v = le_vertice();
 
-	char vertice_procurado = 'b';
+  char *vertice_procurado = malloc(STRING_SIZE * sizeof(char));
+  vertice_procurado = "b";
 
-	char *vert = malloc(sizeof(char));
+  char *vert = malloc(STRING_SIZE * sizeof(char));
 
-	strcpy(vert, &vertice_procurado);
+  strcpy(vert, vertice_procurado);
 
-	printf("vertice = %s\n", &vertice_procurado);	
+  printf("vertice = %s\n", vert);
 
-	coef = coeficiente_proximidade(grafo, &vertice_procurado);
+  coef = coeficiente_proximidade(grafo, vertice_procurado);
+  printf("Coeficiente: %f\n", coef);
 
-	// printf("O coeficiente de proximidade é %f\n", coef);
+  //   printf("O coeficiente de proximidade é %f\n", coef);
 
-	// return !destroi_grafo(g);
-	return 0;
+  // return !destroi_grafo(g);
+  return 0;
 }
