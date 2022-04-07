5a60c1e2aca2add4a4c2950a8940ab020aff7d16
Gustavo Valente <gustavo.pw00@gmail.com>
2022-04-06 23:15:07 -0300

    Update teste.c
    Comentários na main

--- src/teste.c
+++ src/teste.c
@@ -4,18 +4,19 @@
 
 //------------------------------------------------------------------------------
 
-int main(void) {
-  vertice_t v;
-  double coef;
+int main(void)
+{
+	vertice_t v;
+	double coef;
 
-  queue_grafo_t *grafo = le_grafo(stdin);
-  queue_print("Elementos inseridos:\n", (queue_t *)grafo, print_fila);
-  // v = le_vertice();
+	queue_grafo_t *grafo = le_grafo(stdin);
+	// queue_print("Elementos inseridos:\n", (queue_t *)grafo, print_fila);
+	// v = le_vertice();
 
-  // coef = coeficiente_proximidade(grafo, v);
+	// coef = coeficiente_proximidade(grafo, v);
 
-  // printf("O coeficiente de proximidade é %f\n", coef);
+	// printf("O coeficiente de proximidade é %f\n", coef);
 
-  // return !destroi_grafo(g);
-  return 0;
+	// return !destroi_grafo(g);
+	return 0;
 }
